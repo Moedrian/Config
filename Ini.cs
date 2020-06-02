03d317b64d0987f4c03d6e035c2370f05d17fecc
moedrian <ikamitse@gmail.com>
2020-06-03 07:23:02 +0800

    init commit version init.2

---
+++ Ini.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Config
+{
+    class Ini
+    {
+        private readonly string _iniFile;
+        private readonly Regex _sectionMatch = new Regex(@"^\[.*\]$");
+
+        public Ini(string filepath)
+        {
+            _iniFile = filepath;
+        }
+
+
+        public Dictionary<string, Dictionary<string, string>> Read()
+        {
+            var ini = new Dictionary<string, Dictionary<string, string>>();
+            var validLines = new List<string>();
+            var sectionIndex = new List<int>();
+
+            foreach (var line in SplitToLines(_iniFile))
+            {
+                if (line.Trim().StartsWith(";") || line != "")
+                    continue;
+
+                validLines.Add(line);
+            }
+
+            foreach (var line in validLines)
+            {
+                if (_sectionMatch.IsMatch(line))
+                    sectionIndex.Add(validLines.IndexOf(line));
+            }
+
+            return ini;
+        }
+
+        private IEnumerable<string> SplitToLines(string filepath)
+        {
+            if(filepath == null)
+                yield break;
+
+            using (StreamReader reader = new StreamReader(filepath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                    yield return line;
+            }
+        }
+    }
+}
