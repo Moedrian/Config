9cd543d83f395a74bf268474e89adc74555709e0
moedrian <ikamitse@gmail.com>
2020-06-03 14:46:03 +0800

    added Ini.Read() and Ini.Write()
    finished preliminary version of Config.Ini
    updated README.md

--- Ini.cs
+++ Ini.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Config
 {
-    class Ini
+    public class Ini
     {
         private readonly string _iniFile;
         private readonly Regex _sectionMatch = new Regex(@"^\[.*\]$");
@@ -22,25 +22,93 @@ namespace Config
         public Dictionary<string, Dictionary<string, string>> Read()
         {
             var ini = new Dictionary<string, Dictionary<string, string>>();
+
             var validLines = new List<string>();
-            var sectionIndex = new List<int>();
+            var sectionIndices = new List<int>();
+            var sections = new List<IEnumerable<string>>();
 
             foreach (var line in SplitToLines(_iniFile))
             {
-                if (line.Trim().StartsWith(";") || line != "")
+                // To skip comments and empty lines
+                if (line.Trim().StartsWith(";") || line.StartsWith("/") || line.Length == 0)
                     continue;
 
-                validLines.Add(line);
+                if (line.Contains("=") || (line.StartsWith("[") && line.EndsWith("]")))
+                    validLines.Add(line);
             }
 
+            // Get list of section indices
             foreach (var line in validLines)
-            {
                 if (_sectionMatch.IsMatch(line))
-                    sectionIndex.Add(validLines.IndexOf(line));
+                    sectionIndices.Add(validLines.IndexOf(line));
+
+            // manually add the delimiter, which is equal to IndexOfUpperBound + 1
+            sectionIndices.Add(validLines.Count);
+            for (int i = 0; i < sectionIndices.Count - 1; i++)
+            {
+                // ArraySegment provides ability to use offset, and no modification of origin array
+                var sectionSegment =
+                    new ArraySegment<string>(validLines.ToArray(), sectionIndices[i], sectionIndices[i + 1] - sectionIndices[i]);
+                sections.Add(sectionSegment.ToList());
+            }
+
+            // Get the final dictionary containing the whole ini file
+            // The comments of original file will be erased...
+            foreach (var section in sections)
+            {
+                var sectionList = section.ToList();
+                var sectionName = sectionList.First();
+                sectionList.RemoveAt(0);
+                var kvPairs = new Dictionary<string, string>();
+                foreach (var kvPair in sectionList)
+                {
+                    var kv = kvPair.Split('=');
+                    kvPairs.Add(kv[0].Trim(), kv[1].Trim());
+                }
+                ini.Add(sectionName.Trim('[', ']'), kvPairs);
             }
 
             return ini;
         }
+
+
+        // Accepts a 2-D array as argument
+        public void Write(string[,] contents)
+        {
+            var wholeFile = Read();
+
+            for (int i = 0; i < contents.Length; i++)
+            {
+                string section = contents[i, 0];
+                string key = contents[i, 1];
+                string value = contents[i, 2];
+
+                if (wholeFile.ContainsKey(section))
+                {
+                    wholeFile[section][key] = value;
+                }
+                else
+                {
+                    var newKvPair = new Dictionary<string, string>();
+                    newKvPair[key] = value;
+                    wholeFile[section] = newKvPair;
+                }
+            }
+
+            File.WriteAllText(_iniFile, string.Empty);
+            using (var iniWriter = new StreamWriter(_iniFile))
+            {
+                foreach (var sectionPair in wholeFile)
+                {
+                    iniWriter.WriteLine("[" + sectionPair.Key + "]");
+                    foreach (var kv in sectionPair.Value)
+                    {
+                        iniWriter.WriteLine(kv.Key + "=" + kv.Value);
+                    }
+                }
+            }
+        }
+
 
         private IEnumerable<string> SplitToLines(string filepath)
         {
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Config
+
 A C# implementation of reading/writing configuration files.
+
+## Before You Start
+
+Currently this snippet will erase all the comments and empty lines in
+the original ini file. And it cannot validate if the input file is a 
+valid ini file.
+
+## Usage
+
+```C#
+
+using Config;
+
+// first you need to create a new instance
+var iniFile = new Ini(pathToIniFile);
+
+// returns a Dictionary<string, Dictionary<string, string>> variable
+// [section -> [key -> value],],
+var contents = Ini.Read();
+
+// argument be like 
+string[,] toBeWriiten = new string[,] {{section, key, value},}
+Ini.Write(toBeWritten);
+
+```
+
